7f0e0bdba8d799e7a884f0aaf3e010e3a3d4860b
MehriddinBarnoyev <149578450+MehriddinBarnoyev@users.noreply.github.com>
2024-03-18 16:40:15 +0500

    yuklandi
    yuklandi

---
+++ vazifa5/src/App.js
@@ -0,0 +1,158 @@
+import logo from './logo.svg';
+import './App.css';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className='container'>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <header class="py-5 bg-light border-bottom mb-4">
+            <div class="info">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">Welcome to Blog Home!</h1>
+                    <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
+                </div>
+            </div>
+        </header>	
+       
+        <div className="car1d">
+      <div className="card mb-4">
+        <a href="#"><img className='card-img-top'  src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="" /></a>
+        <div className="small text-muted">January 1 2023</div>
+        <h2 className="card-title h4">Post title</h2>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+       <a className='btn btn-primary' href="#">Read more →</a>
+      </div>
+      <div className="card mb-4">
+        <a href="#"><img className='card-img-top'  src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="" /></a>
+        <div className="small text-muted">January 1 2023</div>
+        <h2 className="card-title h4">Post title</h2>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+       <a className='btn btn-primary' href="#">Read more →</a>
+      </div>
+      <div className="card mb-4">
+        <a href="#"><img className='card-img-top'  src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="" /></a>
+        <div className="small text-muted">January 1 2023</div>
+        <h2 className="card-title h4">Post title</h2>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+       <a className='btn btn-primary' href="#">Read more →</a>
+      </div>
+      <div className="card mb-4">
+        <a href="#"><img className='card-img-top'  src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="" /></a>
+        <div className="small text-muted">January 1 2023</div>
+        <h2 className="card-title h4">Post title</h2>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+       <a className='btn btn-primary' href="#">Read more →</a>
+      </div>
+      <div className="card mb-4">
+        <a href="#"><img className='card-img-top'  src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="" /></a>
+        <div className="small text-muted">January 1 2023</div>
+        <h2 className="card-title h4">Post title</h2>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+       <a className='btn btn-primary' href="#">Read more →</a>
+      </div>
+      <div className="card mb-4">
+        <a href="#"><img className='card-img-top'  src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="" /></a>
+        <div className="small text-muted">January 1 2023</div>
+        <h2 className="card-title h4">Post title</h2>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+       <a className='btn btn-primary' href="#">Read more →</a>
+      </div>
+      <div className="card mb-4">
+        <a href="#"><img className='card-img-top'  src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="" /></a>
+        <div className="small text-muted">January 1 2023</div>
+        <h2 className="card-title h4">Post title</h2>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+       <a className='btn btn-primary' href="#">Read more →</a>
+      </div>
+      <div className="card mb-4">
+        <a href="#"><img className='card-img-top'  src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="" /></a>
+        <div className="small text-muted">January 1 2023</div>
+        <h2 className="card-title h4">Post title</h2>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+       <a className='btn btn-primary' href="#">Read more →</a>
+      </div>
+      </div>
+      <div className="col-lg-4">
+          <div className="card mb-4">
+            <div className="card-header">Search</div>
+            <div className="card-body">
+              <div className="input-group">
+                <div className="form-control"
+                typeof='text'
+                aria-label='Search term' 
+                aria-describedby='button-search'
+                placeholder='search'>
+                  
+                  <div className="btn btn-primary" 
+                  id='button-search'
+                  typeof='button'>
+                    Go
+                    </div>
+                </div>
+              </div>
+            
+            
+            </div>
+          </div>
+          <div className="card mb-4">
+                <div className="card-header">Categories</div>
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-cm-6">
+                      <ul className='list-unstyled mb-0'>
+                        <li> <a href="#"> Web design</a></li>
+                        <li> <a href="#">HTML</a></li>
+                        <li><a href="#">CSS</a></li>
+                      </ul>
+                    </div>
+                    <div className="col-cm-6">
+                      <ul className="list-unstyled mb-0">
+                        <li><a href="#">JavaScript</a></li>
+                        <li><a href="#">Java</a></li>
+                        <li><a href="#">Spring</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          <div className="card mb-4">
+                <div className="card-header">Side widget</div>
+                <div className="card-body">You can put anything you want</div>
+              </div>
+          </div>  
+          <nav aria-label="Pagination">
+                        <hr  className="my-0" />
+                        <ul class="pagination justify-content-center my-4">
+                            <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1" aria-disabled="true">Newer</a></li>
+                            <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">3</a></li>
+                            <li class="page-item disabled"><a class="page-link" href="#!">...</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">15</a></li>
+                            <li class="page-item"><a class="page-link" href="#!">Older</a></li>
+                        </ul>
+                    </nav>
+                    <footer>
+                      <div className="text text-expand-lg text-dark bg-dark text-center ">
+                        <p>Copyright © Your Website 2023</p>
+                      </div>
+                    </footer>
+    </div>
+  );
+}
+
+export default App;
